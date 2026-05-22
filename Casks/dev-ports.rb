57061e2a8c2968ce141c9f60c46cdc9dd3563819
邵高杰 <shaogaojie@gmail.com>
2026-05-22 15:44:27 +0800

    Update dev-ports to 0.1.2

--- Casks/dev-ports.rb
+++ Casks/dev-ports.rb
@@ -2,13 +2,13 @@ cask "dev-ports" do
   version "0.1.2"
   sha256 "37b63039227afe421654aac929a3328323d45b9a06950df25fc962b17b7228c9"
 
-  url "https://github.com/igaojie/dev-ports/releases/download/v0.1.2/dev-ports-0.1.2-arm64.dmg"
+  url "https://github.com/igaojie/dev-ports-releases/releases/download/v0.1.2/dev-ports-0.1.2-arm64.dmg"
   name "dev-ports"
   desc "Local multi-project development port ledger and desktop inspector"
-  homepage "https://github.com/igaojie/dev-ports"
+  homepage "https://github.com/igaojie/dev-ports-releases"
 
   livecheck do
-    url "https://github.com/igaojie/dev-ports"
+    url "https://github.com/igaojie/dev-ports-releases"
     regex(/^v?(\d+(?:\.\d+)+)$/i)
     strategy :github_latest
   end
